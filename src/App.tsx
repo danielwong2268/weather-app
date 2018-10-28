e63747544e5eaf0b54684742febe77661364e207
Daniel Wong <danielwong2268@gmail.com>
2018-10-27 21:42:24 -0700

    setup some dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
+import styled from 'react-emotion';
 import './App.css';
+import 'antd/dist/antd.css'; 
 
-import logo from './logo.svg';
+const Div = styled('div')`
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  justify-items: center;
+  padding: 40px;
+`;
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Div className="App">
+        <div>some content</div>
+      </Div>
     );
   }
 }
